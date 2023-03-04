64e931f9b6b169568f093d778c0846b8570129ef
takaaki12353491 <takaaki12353491@gmail.com>
2023-03-04 17:18:36 +0900

    feat: memoryモジュールを作成

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use crate::memory::_stack_overflow;
+
 // modで読み込まないとrust-analyzerが効かない場合がある
+mod memory;
 mod module;
 mod variable;
 
 fn main() {
     // !はマクロ
     println!("Hello, world!");
+    _stack_overflow();
 }
 
 // 定義のみで使用しない関数や変数には先頭に_をつける
